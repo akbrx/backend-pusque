974e6ffca78559bc304f31ae1bbf3e4c99241ccf
akbrx <strongstroomer@gmail.com>
2025-06-13 14:43:45 +0700

    env update

--- config/database.js
+++ config/database.js
@@ -2,22 +2,13 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-    logging: false,
-    pool: {
-      max: 20,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  }
-);
+const sequelize = new Sequelize({
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABSE,
+  dialect: 'mysql',
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+});
 
-export default db;
+module.exports = sequelize;

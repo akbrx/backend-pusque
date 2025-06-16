1a9299e878cfb25bf8f59e584b589444cbc2bdd2
akbrx <strongstroomer@gmail.com>
2025-06-16 23:03:41 +0700

    server.js

--- server.js
+++ server.js
@@ -9,21 +9,23 @@ import cors from 'cors';
 import Antrian from './antrian/antrian-model.js'; //idupkan ini jika ingin membuat tabel antrian
 import Users from './users/user-model.js'; //idupkan ini jika ingin membuat tabel users
 import Feedback from './feedback/feedback-model.js';
+const fs = require('fs');
+const path = require('path');
 
 dotenv.config();
 const app = express();
 
 
 // Fungsi untuk membuat direktori jika belum ada
-    const createUploadsDirectory = () => {
-        const uploadDir = path.join(process.cwd(), 'uploads', 'ktp');
-        if (!fs.existsSync(uploadDir)) {
-            fs.mkdirSync(uploadDir, { recursive: true });
-            console.log(`Direktori "${uploadDir}" berhasil dibuat.`);
-        } else {
-            console.log(`Direktori "${uploadDir}" sudah ada.`);
-        }
-    };
+const createUploadsDirectory = () => {
+    const uploadDir = path.join(process.cwd(), 'uploads', 'ktp');
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir, { recursive: true });
+        console.log(`Direktori "${uploadDir}" berhasil dibuat.`);
+    } else {
+        console.log(`Direktori "${uploadDir}" sudah ada.`);
+    }
+};
 
 try {
   await db.authenticate();
@@ -31,7 +33,7 @@ try {
 
   createUploadsDirectory();
 
-  //await db.sync(); 
+  await db.sync(); 
   await Users.sync(); 
   await Antrian.sync(); 
   await Feedback.sync(); 
